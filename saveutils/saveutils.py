4e96ed5d624b491abd3b6a428b54f735c40f7ed8
Alexandre Potvin-Demers <alexpotvind@icloud.com>
2020-12-18 17:43:41 -0500

    Added function get_next_filename

--- saveutils/saveutils.py
+++ saveutils/saveutils.py
@@ -3,12 +3,26 @@ Image file save utilities
 """
 
 import os
+import re
 
 
-def get_next_filename(base_path: str = "/saved_images/"):
+def get_next_filename(base_path: str = "saved_images/"):
     """
     Generates the filename for the next image to be saved
     :param base_path: The base filepath to save the images to
     :return: String: The path of the next image file to be saved
     """
-    pass
+
+    # Listing files matching IMG_XXXXX.jpg pattern
+    file_nums = [x.split('_')[-1] for x in os.listdir(base_path) if re.match(r"IMG_[0-9]{5}\.jpg", x)]
+    # Keeping only image file number
+    file_nums = [x.split('.')[0] for x in file_nums]
+    # Casting list elements to integer
+    file_nums = [int(x) for x in file_nums]
+
+    # Getting the next integer to use as a filename
+    next_file_number_str = str(max(file_nums) + 1)
+    while len(next_file_number_str) != 5:
+        next_file_number_str = "0" + next_file_number_str
+
+    return base_path + "IMG_" + next_file_number_str + ".jpg"
